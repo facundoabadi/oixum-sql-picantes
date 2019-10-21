cf3f04ef829173c512c146f2ac880ff9ab1b37c4
Your Name <ramispano@gmail.com>
2019-10-21 17:44:23 -0300

    cambios

---
+++ src/SpanoAbadiValle.sql
@@ -0,0 +1,6 @@
+insert into test.estudiantes (nombre , comidaPreferida , fechaNacimiento)
+values ('Federico Aloi', 'Pastel de papa', '1991-10-30'), 
+       ('Ramiro Spano' , 'Milanesas Napolitanas', '2002-08-29'),
+	   ('Facundo valle','Fideos Caseros','2001-08-30');
+
+
