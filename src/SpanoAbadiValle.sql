a370c816483b4c105709c3f55d507947216c4eea
Ramiro <48453907+ramispano@users.noreply.github.com>
2019-11-11 16:06:17 -0300

    Delete SpanoAbadiValle.sql

--- src/SpanoAbadiValle.sql
+++
@@ -1,6 +0,0 @@
-insert into test.estudiantes (nombre , comidaPreferida , fechaNacimiento)
-values ('Federico Aloi', 'Pastel de papa', '1991-10-30'), 
-       ('Ramiro Spano' , 'Milanesas Napolitanas', '2002-08-29'),
-	   ('Facundo valle','Fideos Caseros','2001-08-30');
-
-
