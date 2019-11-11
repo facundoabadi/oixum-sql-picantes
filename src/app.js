b4983bc7c0eacb7cc0513a809c078df2468708af
Ramiro <48453907+ramispano@users.noreply.github.com>
2019-11-11 16:02:18 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var connection = require('./connection');
+var exphbs = require('express-handlebars');
+
+
+var path = require('path');
 
 var app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.send('¡Hola ET 35!');
+  res.render('home');
 });
 
-app.get('/query', function(req, res) {
-  connection.query('SELECT * FROM estudiantes', function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
+app.post('/', function(req, res) {
+  const query = req.body.query;
+  connection.query(query, function (error, results, fields) {
+
+    res.render('home', {
+      resultados: JSON.stringify(results),
+      error: error,
+      text: query
+
+    });
   });
 });
 
